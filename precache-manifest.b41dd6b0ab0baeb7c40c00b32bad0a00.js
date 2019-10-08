f76aacc5ce4497bc247aedbe21aaddc0912139bb
LoganWdev <loganwdev@gmail.com>
2019-10-08 15:50:03 -0500

    Updates

---
+++ precache-manifest.b41dd6b0ab0baeb7c40c00b32bad0a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428c42893ff44f37a344e788d2e37a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "721a0cc84f72b27306e4",
+    "url": "/static/css/main.991f5cde.chunk.css"
+  },
+  {
+    "revision": "0553455aef52e8d1856d",
+    "url": "/static/js/2.29d05229.chunk.js"
+  },
+  {
+    "revision": "721a0cc84f72b27306e4",
+    "url": "/static/js/main.3f8cfadd.chunk.js"
+  },
+  {
+    "revision": "7915e3da98a50815a284",
+    "url": "/static/js/runtime-main.6e3bd318.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);

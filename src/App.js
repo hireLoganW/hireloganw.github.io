1df08500dcd15f616cc88bbc2745c48f8bf226db
LoganWdev <loganwdev@gmail.com>
2019-10-08 14:53:57 -0500

    maybeworksnow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [{
+
+id:1,
+title:"spaceJam 1"
+},
+{
+ id:2,
+ title:"spaceJam 2" 
+},{
+  id:3,
+  title:"spaceJam 3" 
+ }];
+
+class App extends Component {
+  
+  render() { 
+    
+   return (
+     <div className="App">
+      <header className="App-header" style={{color:'green'}}>
+        {/* below I am iterating over an array. mapping the array of objects -> to a ("./Movie.js" - Component).  w/ the keys of movie's[id,title] */}
+          {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+
+        {/*Company Logo */}
+          <img src={logo} className="App-logo" alt="logo"/>
+          
+       </header> 
+      
+       
+        
+        
+     </div>
+   );
+  }
 }
 
 export default App;
